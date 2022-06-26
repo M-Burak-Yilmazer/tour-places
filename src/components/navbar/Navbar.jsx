cc654653e599908522f28c3dd1ba5e1ab3ffefd4
burak <mbyilmazer38@gmail.com>
2022-06-27 02:08:50 +0300

    navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import "./Navbar.scss";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+    <a href="#aboutus">About Us</a>
+    <a href="#foryou">For You</a>
+    <a href="#services">Services</a>
+    <a href="#blog">Blog</a>
+    <a href="#vlog">Vlog</a>
+    <a href="#contact">Contact</a>
+
+
+    </div>
+  )
+}
+
+export default Navbar;
+
+// import './Navbar.scss';
+
+
+
+// const Navbar = () => {
+//     return(
+//         <div className="navbar">
+//             <a href="#aboutus">About Us</a>
+//             <a href="#foryou">For You</a>
+//             <a href="#services">Services</a>
+//             <a href="#blog">Blog</a>
+//             <a href="#vlog">Vlog</a>
+//             <a href="#contact">Contact</a>
+//         </div>
+//     )
+// }
+
+// export default Navbar;
